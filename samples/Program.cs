4344513f2a2ce3e5e4168a3036f7e76545763d1d
Secured Signing <support@securedsigning.com>
2015-10-07 17:15:32 +1300

    Add a sample project.

---
+++ samples/Program.cs
@@ -0,0 +1,24 @@
+﻿using SecuredSigningClientSdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new ServiceClient(
+                serviceUrl: "https://api.securedsigning.com/web",
+                version: "v1.3",
+                apiKey: "YOUR API KEY",
+                secret: "YOUR API Secret",
+                accessUrl: "YOUR Access URL");
+
+            var forms = client.getFormList();
+        }
+    }
+}
